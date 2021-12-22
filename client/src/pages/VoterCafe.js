4d02a7acc12b90f709af6b0cc23c7ac782080ba4
Shlok1999 <47269110+Shlok1999@users.noreply.github.com>
2021-12-22 16:48:49 +0530

    Added changes after vote being casted in VoterCafe.js

--- client/src/pages/VoterCafe.js
+++ client/src/pages/VoterCafe.js
@@ -25,7 +25,14 @@ const VoterCafe = props => {
     case 2:
       return <Vote />;
     case 3:
-      return <MessageComponent msg='Results will be declared soon ...' />;
+      return (
+        <Link
+            to='/'
+            className='w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10'
+          >
+            Go To Home
+        </Link>
+        <MessageComponent msg='Results will be declared soon ...' />;)
     // case 3:
     //   return <Leaderboard contract={props.contract} />;
     case 4:
